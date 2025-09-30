f9441ac6149978ac32973390bf6e8bb725260ee3
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-09-30 10:51:27 -1200

    Task1

---
+++ Tyuiu.AkopovaLV.Sprint1.Task1.V6/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.AkopovaLV.Sprint1.Task1.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Акопова Л. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #0                                                              *");
+        Console.WriteLine("* Выполнила: Акопова Людмила Владимировна | АСОиУб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет следующее выражение               *");
+        Console.WriteLine("* 10 + (9/3).                                                             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("РЕЗУЛЬТАТ:                                                                *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x, y));
+
+        Console.ReadLine();
+    }
+}
