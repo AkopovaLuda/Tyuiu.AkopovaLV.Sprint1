9e5131ca6bccf8667c7c8f7880f7476431c950f9
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-09-30 15:23:51 -1200

    Task5

---
+++ Tyuiu.AkopovaLV.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AkopovaLV.Sprint1.Task5.V7.Lib;
+namespace Tyuiu.AkopovaLV.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double f = 90;
+            int result = ds.AngleToHoursMinutes(f);
+            int wait = 3;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
