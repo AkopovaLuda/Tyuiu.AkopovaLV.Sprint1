1361db9e05ca32b12044178f9e46ae48eb30e71b
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-10-21 04:51:11 -1200

    Task3

---
+++ Tyuiu.AkopovaLV.Sprint1.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.AkopovaLV.Sprint1.Task3.V10.Lib
+{
+    public class DataService : ISprint1Task3V10
+    {
+        public string NumberToMoney(double number)
+        {
+            int rub = (int)number;
+            int cop = (int)((number - rub) * 100);
+            return $"{rub} руб. {cop} коп.";
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint1.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AkopovaLV.Sprint1.Task3.V10.Lib;
+
+namespace Tyuiu.AkopovaLV.Sprint1.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double num = 26.6;
+            var res = ds.NumberToMoney(num);
+            Assert.AreEqual("26 руб. 60 коп.", res);
+        }
+    }
+}
